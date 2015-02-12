345ed9899a56e1b85d0a889c2997296ff7ac716d
Eric Katz <ericpkatz@gmail.com>
2015-02-12 02:17:00 +0000

    stats

---
+++ test/stats_spec.js
@@ -0,0 +1,16 @@
+/* globals describe it expect */
+describe("Stats", function(){
+    it("exists", function(){
+        expect(Stats).toBeDefined();
+    });
+    
+    describe("Stats for [1,1,3,7", function(){
+        it("should be {1:2, 3:1, 7:1}", function(){
+           expect(Stats.generate([1,1,3,7])).toEqual({
+               1: 2,
+               3: 1,
+               7: 1
+           });
+        })
+    });
+});
